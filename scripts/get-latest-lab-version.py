2cdd560c3fe0a5c3ed6bb658d280b33f1e5cc733
Vishnutheep B <vishnutheep@gmail.com>
2024-02-28 00:03:21 +0530

    Update get-latest-lab-version.py

--- scripts/get-latest-lab-version.py
+++ scripts/get-latest-lab-version.py
@@ -1,7 +1,7 @@
-from urllib.request import urlopen
-import json
 import argparse
+import sys
 
+import requests
 
 REPOSITORY = "jupyterlab"
 ORGANIZATION = "jupyterlab"
@@ -9,36 +9,48 @@ ORGANIZATION = "jupyterlab"
 
 def extract_version_from_releases(releases, version_tag):
     for release in releases:
-        tag_name = release['tag_name']
-        if (version_tag == "latest"):
-            if(not release['prerelease'] and not release['draft']):
+        tag_name = release["tag_name"]
+        if version_tag == "latest":
+            if not release["prerelease"] and not release["draft"]:
                 return tag_name
-        
-        elif (version_tag == tag_name):
+
+        elif version_tag == tag_name:
             return tag_name
     return None
 
 
 def find_version(owner, repository, version_tag):
-    """Find latest stable release on GitHub for given repository."""
-    endpoint = f"https://api.github.com/repos/{owner}/{repository}/releases"
-    releases = json.loads(urlopen(endpoint).read())
+    url = f"https://api.github.com/repos/{owner}/{repository}/releases"
 
-    version = extract_version_from_releases(releases,version_tag)
+    response = requests.get(url, timeout=10)
+
+    if response.status_code != 200:
+        error_message = (
+            f"Failed to fetch package.json from {url}. HTTP status code: {response.status_code}"
+        )
+        raise requests.HTTPError(error_message)
+
+    releases = response.json()
+    version = extract_version_from_releases(releases, version_tag)
 
     if version is None:
-        raise ValueError('Invalid release tag')
-    if not version.startswith('v'):
-        raise ValueError('Unexpected release tag name format: does not start with v')
+        error_message = "Invalid release tag"
+        raise ValueError(error_message)
     return version[1:]
 
-def main():
-    parser = argparse.ArgumentParser(description='Update dependencies in package.json.')
-    parser.add_argument('--set-version', dest='version_tag', type=str, required=True, help='Set version tag')
-    
-    args = parser.parse_args()
-    print(find_version(owner=ORGANIZATION,repository=REPOSITORY,version_tag=args.version_tag))
-    
 
-if __name__ == '__main__':   
+def main():
+    parser = argparse.ArgumentParser(description="Update dependencies in package.json.")
+    parser.add_argument(
+        "--set-version", dest="version_tag", type=str, required=True, help="Set version tag"
+    )
+
+    args = parser.parse_args()
+
+    result = find_version(owner=ORGANIZATION, repository=REPOSITORY, version_tag=args.version_tag)
+    sys.stdout.write(result)
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
     main()
